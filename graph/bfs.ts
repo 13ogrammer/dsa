0b408cae29fe6ce58d15761942849736cff56bdb
13ogrammer <tasker.prabhat@gmail.com>
2025-05-12 22:52:23 +1000

    Add dfs graph traversal

---
+++ graph/bfs.ts
@@ -0,0 +1,56 @@
+/**
+ * Breadth-First Search (BFS) algorithm
+ * Used for traversing or searching tree or graph data structures
+ *
+ * Time complexity: O(V + E), where V -> # of vertices and E -> number of edges
+ * Space complexity: O(V) for the queue
+ */
+
+export function bfs(
+  graph: Record<string, string[]>,
+  start: string,
+  target?: string
+): string[] | void {
+  const visited = new Set<string>();
+  const queue: string[] = [start];
+  const result: string[] = [];
+
+  while (queue.length > 0) {
+    const node = queue.shift()!;
+
+    if (target && node === target) {
+      console.log(`Target found: ${node}`);
+      console.log(`BFS traversal:`, [...visited, node]);
+      return;
+    }
+
+    if (!visited.has(node)) {
+      visited.add(node);
+      result.push(node);
+
+      for (const neighbor of graph[node] || []) {
+        if (!visited.has(neighbor)) {
+          queue.push(neighbor);
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
+console.time(`execution time`);
+const graph = {
+  A: ["B", "C"],
+  B: ["D", "E"],
+  C: ["F"],
+  D: [],
+  E: ["F"],
+  F: [],
+};
+const startNode = "A";
+const result = bfs(graph, startNode);
+console.log(`BFS traversal starting from node ${startNode}:`, result);
+const targetNode = "B";
+bfs(graph, startNode, targetNode);
+console.timeEnd(`execution time`);
